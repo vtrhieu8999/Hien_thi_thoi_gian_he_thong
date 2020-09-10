12b1bf014403ed119b4e85dcd10a557b4b17c720
Vuong_Trung_Hieu <v.hieu1998@gmail.com>
2020-09-10 10:43:07 +0700

    Hien thi thoi gian he thong

---
+++ src/Hien_thi_thoi_gian_he_thong.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Hien_thi_thoi_gian_he_thong {
+    public static void main(String[] args){
+        Date now= new Date();
+        System.out.println("Now is: "+ now);
+    }
+}
